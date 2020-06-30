20e3d75909180640962d6c7da8961af39ba325c3
liuxin <274250917@qq.com>
2020-06-30 20:19:36 +0800

    'fixbug'

--- LoadSpeed/PhoneUtils.py
+++ LoadSpeed/PhoneUtils.py
@@ -2,45 +2,48 @@
 # -*- coding: UTF-8 -*-
 import subprocess
 import os
-from LoadSpeed import FFMpegUtil
+from LoadSpeed import ffmpeg
 from LoadSpeed import Utils
 
-def PullVideoFile():
-    videoFileName = ""
+BASE_PATH=os.path.dirname(os.path.dirname(__file__))
 
+
+def pull_video_file():
+
+    videoFileName = ""
     p = subprocess.Popen('adb shell ls -l /sdcard/DCIM/Camera/', shell=True, stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
+                         stderr=subprocess.STDOUT,encoding='gbk')
 
     picfiles = p.stdout.readlines()
     picfiles.reverse()
 
     for line in picfiles:
-        if line.strip().endswith('.mp4') != False:
+        print(line)
+        if line.strip().endswith('.mp4'):
             videoFileName = line[line.rindex(' '):].strip()
             break
 
-    if videoFileName != "":
-        videoFileName = "".join(videoFileName.split())
-
-        filedir = "../result/video"
-
-        if os.path.exists(filedir) == False:
+    if videoFileName:
+        print(f'videoFileName:{videoFileName}')
+        filedir = f"{BASE_PATH}/result/video"
+        if not os.path.exists(filedir):
             os.makedirs(filedir)
-        subprocess.call("adb pull /sdcard/DCIM/Camera/" + videoFileName + " " + filedir)
+        subprocess.call(f"adb pull /sdcard/DCIM/Camera/{videoFileName} {filedir}")
     else:
         print("error:can not find video file")
-
     return videoFileName.strip('.mp4')
+
+
 
 
 if __name__ == '__main__':
     print ('开始导出')
-    videoFileName = PullVideoFile()
-    print ('视频文件（%s）导出完成.................................'%videoFileName)
-    print ('开始视频分帧处理..........................................')
-    ff = FFMpegUtil.FFMpegUtil()
-    ff.runFFMpeg(videoFileName)
-    path=ff.getConparaPicPath()+os.path.sep+videoFileName
+    video_name = pull_video_file()
+    print (f'视频文件({video_name})导出完成{"-"*30}')
+    print (f'开始视频分帧处理{"-"*30}')
+    ff = ffmpeg.FFMpegUtil()
+    ff.runFFMpeg(video_name)
+    path=ff.getConparaPicPath()+os.path.sep+video_name
     Utils.rename(path)
 
     print ('分帧处理完成..............................................')
